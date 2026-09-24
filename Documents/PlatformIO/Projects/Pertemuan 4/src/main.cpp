9e5ad13713c769afb436489e71fe28d966910cb7
donnyzakihuda <donnyzakihuda@mail.ugm.ac.id>
2026-09-24 20:28:43 +0700

    Update main.cpp

--- Documents/PlatformIO/Projects/Pertemuan 4/src/main.cpp
+++ Documents/PlatformIO/Projects/Pertemuan 4/src/main.cpp
@@ -1,18 +1,53 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_INA219.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-// put function declarations here:
-int myFunction(int, int);
+Adafruit_INA219 ina219;
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
+#define BUZZER_PIN 26
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+  Serial.begin(9600);
+  Wire.begin(21, 22);
+  ina219.begin();
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  pinMode(BUZZER_PIN, OUTPUT);
+} 
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  float arus = ina219.getCurrent_mA();
+  float tegangan_V = ina219.getBusVoltage_V();
+  float daya_mW = ina219.getPower_mW();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (arus > 500) {
+    digitalWrite(BUZZER_PIN, HIGH);
+    delay(500);
+    
+  } else {
+    digitalWrite(BUZZER_PIN, LOW);
+    delay(500);
+  }
+
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.println("===========");
+  display.print("Tegangan(V): ");
+  display.println(tegangan_V);
+  display.print("Arus(mA): ");
+  display.println(arus);
+  display.print("Daya(mW): ");
+  display.println(daya_mW);
+  display.display();
+  
+  Serial.println("===========");
+  Serial.print("Tegangan(V): ");
+  Serial.println(tegangan_V);
+  Serial.print("Arus(mA): ");
+  Serial.println(arus);
+  Serial.print("Daya(mW): ");
+  Serial.println(daya_mW);
+  delay(1000);
 }
